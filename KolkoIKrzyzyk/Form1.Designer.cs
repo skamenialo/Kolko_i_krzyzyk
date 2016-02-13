76bed2c9b2905e0b8275ca233cbd94cacb5abb55
Daniel Janowski <janowski.d@wp.pl>
2016-02-13 01:39:54 +0100

    Add project

---
+++ KolkoIKrzyzyk/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace KolkoIKrzyzyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+      System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.graczVsSIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.sIVsSIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.AllowUserToAddRows = false;
+      this.dataGridView1.AllowUserToDeleteRows = false;
+      this.dataGridView1.AllowUserToResizeColumns = false;
+      this.dataGridView1.AllowUserToResizeRows = false;
+      this.dataGridView1.ColumnHeadersHeight = 20;
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+      this.dataGridView1.ColumnHeadersVisible = false;
+      this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+      dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+      dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+      dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+      dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+      dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+      dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+      this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+      this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+      this.dataGridView1.MultiSelect = false;
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.ReadOnly = true;
+      this.dataGridView1.RowHeadersVisible = false;
+      this.dataGridView1.RowHeadersWidth = 20;
+      this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+      dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+      dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
+      this.dataGridView1.RowTemplate.Height = 40;
+      this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+      this.dataGridView1.Size = new System.Drawing.Size(198, 198);
+      this.dataGridView1.TabIndex = 0;
+      this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.restartToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(222, 24);
+      this.menuStrip1.TabIndex = 1;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // plikToolStripMenuItem
+      // 
+      this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.graczVsSIToolStripMenuItem,
+            this.sIVsSIToolStripMenuItem,
+            this.exitToolStripMenuItem});
+      this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+      this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+      this.plikToolStripMenuItem.Text = "Plik";
+      // 
+      // graczVsSIToolStripMenuItem
+      // 
+      this.graczVsSIToolStripMenuItem.Name = "graczVsSIToolStripMenuItem";
+      this.graczVsSIToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+      this.graczVsSIToolStripMenuItem.Text = "Gracz vs SI";
+      this.graczVsSIToolStripMenuItem.Click += new System.EventHandler(this.graczVsSIToolStripMenuItem_Click);
+      // 
+      // sIVsSIToolStripMenuItem
+      // 
+      this.sIVsSIToolStripMenuItem.Name = "sIVsSIToolStripMenuItem";
+      this.sIVsSIToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+      this.sIVsSIToolStripMenuItem.Text = "SI vs SI";
+      this.sIVsSIToolStripMenuItem.Click += new System.EventHandler(this.sIVsSIToolStripMenuItem_Click);
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+      this.exitToolStripMenuItem.Text = "Exit";
+      this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+      // 
+      // restartToolStripMenuItem
+      // 
+      this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+      this.restartToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+      this.restartToolStripMenuItem.Text = "Restart";
+      this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click_1);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.label1.Location = new System.Drawing.Point(60, 234);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(56, 20);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "Gracz:";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.label2.Location = new System.Drawing.Point(122, 234);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(0, 20);
+      this.label2.TabIndex = 3;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.ClientSize = new System.Drawing.Size(222, 263);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.dataGridView1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.MaximizeBox = false;
+      this.MaximumSize = new System.Drawing.Size(238, 302);
+      this.MinimizeBox = false;
+      this.MinimumSize = new System.Drawing.Size(238, 302);
+      this.Name = "Form1";
+      this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+      this.Text = "Kółko i krzyżyk";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem graczVsSIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sIVsSIToolStripMenuItem;
+    }
+}
+
---
+++ KolkoIKrzyzyk/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KolkoIKrzyzyk
+{
+    public partial class Form1 : Form
+    {
+        const Int32 k = 3;
+        Char[][] t;
+        Char gracz;
+        Boolean siX, siO;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            siX = false;
+            siO = false;
+            Random r = new Random();
+            if (r.Next(2) == 0)
+                gracz = 'O';
+            else
+                gracz = 'X';
+            for (int i = 0; i < k; i++)
+            {
+                AddAColumn(i);
+            }
+            dataGridView1.RowHeadersDefaultCellStyle.Padding = new Padding(3);
+            for (int i = 0; i < k; i++)
+            {
+                AddARow(i);
+            }
+
+            t = new Char[dataGridView1.RowCount][];
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = new Char[dataGridView1.ColumnCount];
+                for (int j = 0; j < t[i].Length; j++)
+                {
+                    t[i][j] = ' ';
+                    dataGridView1.Rows[i].Cells[j].Value = t[i][j].ToString();
+                }
+            }
+            label2.Text = gracz.ToString();
+            dataGridView1.ClearSelection();
+        }
+        private void AddARow(int i)
+        {
+            DataGridViewRow Arow = new DataGridViewRow();
+            Arow.HeaderCell.Value = i.ToString();
+            Arow.Height = dataGridView1.Height / k;
+            dataGridView1.Rows.Add(Arow);
+        }
+        private void AddAColumn(int i)
+        {
+            DataGridViewTextBoxColumn Acolumn = new DataGridViewTextBoxColumn();
+            Acolumn.Name = i.ToString();
+            Acolumn.Width = dataGridView1.Width / k;
+            Acolumn.SortMode = DataGridViewColumnSortMode.NotSortable;
+            dataGridView1.Columns.Add(Acolumn);
+        }
+
+        void Restart()
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = "";
+                    t[i][j] = ' ';
+                }
+            }
+            dataGridView1.Enabled = true;
+            Random r = new Random();
+            if (r.Next(2) == 0)
+            {
+                gracz = 'O';
+            }
+            else
+            {
+                if (siX && !siO)
+                {
+                    Ruch(t, 'X');
+                }
+                else
+                {
+                    gracz = 'X';
+                }
+            }
+            label1.Text = "Gracz";
+            label2.Text = gracz.ToString();
+            if (siX && siO)
+                sIVsSI();
+        }
+
+        bool Wygrana(Char[][] t, Char g, bool cisza)
+        {
+            bool test = false;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (test == false)
+                {
+                    test = (t[i][0] == g && t[i][1] == g && t[i][2] == g);
+                }
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (test == false)
+                {
+                    test = (t[0][i] == g && t[1][i] == g && t[2][i] == g);
+                }
+            }
+            if (test == false)
+            {
+                test = true;
+                for (int i = 0; i < t.Length; i++)
+                {
+                    test = (test && t[i][i] == g);
+                }
+            }
+            if (test == false)
+            {
+                test = true;
+                for (int i = 0, j = t[i].Length - 1; i < t.Length; i++, j--)
+                {
+                    test = (test && t[i][j] == g);
+                }
+            }
+            if (test)
+            {
+                if (!cisza)
+                {
+                    label1.Text = "Wygrywa";
+                }
+            }
+            return test;
+        }
+        bool Remis(Char[][] t, bool cisza)
+        {
+            for (int i = 0; i < t.Length; i++)
+                for (int j = 0; j < t[i].Length; j++)
+                    if (t[i][j] == ' ')
+                        return false;
+
+            if (!cisza)
+            {
+                label1.Text = "Remis";
+                label2.Text = "";
+            }
+            return true;
+        }
+        int MiniMax(ref List<Point> l, Char[][] t, Char g, int poz)
+        {
+            int m, mmx;
+            mmx = (g == 'X') ? 2 : -2;
+            for (int i = 0; i < t.Length; i++)
+            {
+                for (int j = 0; j < t[i].Length; j++)
+                {
+                    if (t[i][j] == ' ')
+                    {
+                        t[i][j] = g;
+                        if (Wygrana(t, g, true))
+                        {
+                            m = (g == 'X') ? -1 : 1;
+                        }
+                        else if (Remis(t, true))
+                        {
+                            m = 0;
+                        }
+                        else
+                        {
+                            m = MiniMax(ref l, t, (g == 'X') ? 'O' : 'X', poz + 1);
+                        }
+                        if (((g == 'X') && (m < mmx)) || ((g == 'O') && (m > mmx)))
+                        {
+                            mmx = m;
+                            if (!Convert.ToBoolean(poz))
+                            {
+                                l.Clear();
+                            }
+                        }
+                        if (m == mmx)
+                        {
+                            if (!Convert.ToBoolean(poz))
+                            {
+                                l.Add(new Point(i, j));
+                            }
+                        }
+                        t[i][j] = ' ';
+                    }
+                }
+            }
+            return mmx;
+        }
+        void Ruch(Char[][] t, Char g)
+        {
+            List<Point> lista = new List<Point>();
+            Point wybor;
+            MiniMax(ref lista, t, g, 0);
+            Random rand = new Random();
+            wybor = lista[rand.Next(lista.Count)];
+            t[wybor.X][wybor.Y] = g;
+            dataGridView1.Rows[wybor.X].Cells[wybor.Y].Value = g.ToString();
+            if (!Wygrana(t, 'X', false) && !Wygrana(t, 'O', false))
+            {
+            }
+            else if (Remis(t, false))
+            {
+            }
+            else
+            {
+                label2.Text = g.ToString();
+                dataGridView1.Enabled = false;
+            }
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (t[e.RowIndex][e.ColumnIndex] == ' ')
+            {
+
+                dataGridView1.CurrentCell.Value = gracz;
+                t[e.RowIndex][e.ColumnIndex] = Convert.ToChar(gracz);
+                if (!Wygrana(t, 'X', false) && !Wygrana(t, 'O', false) && !Remis(t, false))
+                {
+                    if (gracz == 'O')
+                        if (siX == false)
+                        {   
+                            gracz = 'X';
+                            label2.Text = gracz.ToString();
+                        }
+                        else
+                        {
+                            Ruch(t, 'X');
+                        }
+                    else
+                    {
+                        if (siO == false)
+                        {
+                            gracz = 'O';
+                            label2.Text = gracz.ToString();
+                        }
+                        else
+                            Ruch(t, 'O');
+                    }
+                }
+                else
+                {
+                    dataGridView1.Enabled = false;
+                }
+            }
+            dataGridView1.CurrentCell.Selected = false;
+        }
+        async void sIVsSI()
+        {
+            bool test = true;
+            do
+            {
+                await Task.Delay(1000);
+                if (!Wygrana(t, 'X', false) && !Wygrana(t, 'O', false) && !Remis(t, false))
+                {
+                    if (gracz == 'O')
+                    {
+                        Ruch(t, 'O');
+                        gracz = 'X';
+                    }
+                    else
+                    {
+                        Ruch(t, 'X');
+                        gracz = 'O';
+                    }
+                }
+                else
+                {
+                    test = false;
+                }
+
+            } while (test);
+        }
+        private void graczVsSIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!siX || siO)
+            {
+                siX = true;
+                siO = false;
+                graczVsSIToolStripMenuItem.Checked = true;
+                sIVsSIToolStripMenuItem.Checked = false;
+                Restart();
+            }
+            else
+            {
+                siX = false;
+                siO = false;
+                graczVsSIToolStripMenuItem.Checked = false;
+                sIVsSIToolStripMenuItem.Checked = false;
+                Restart();
+            }
+        }
+
+        private void restartToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Restart();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Enabled)
+            {
+                if (MessageBox.Show("Czy napewno chcesz przerwać grę?", "Zakończ", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    this.Close();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void sIVsSIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!siX && !siO)
+            {
+                siX = true;
+                siO = true;
+                graczVsSIToolStripMenuItem.Checked = false;
+                sIVsSIToolStripMenuItem.Checked = true;
+                Restart();
+                sIVsSI();
+            }
+            else
+            {
+                siX = false;
+                siO = false;
+                graczVsSIToolStripMenuItem.Checked = false;
+                sIVsSIToolStripMenuItem.Checked = false;
+                Restart();
+            }
+        }
+    }
+}
